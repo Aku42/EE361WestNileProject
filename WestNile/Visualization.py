77eb50db9ebe295bfac043427614bc7f198a947e
chargao <charlie.gao@utexas.edu>
2016-05-03 09:26:41 -0500

    maps pt 1

--- WestNile/Visualization.py
+++ WestNile/Visualization.py
@@ -8,6 +8,9 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.gaussian_process import GaussianProcess
 from sklearn.linear_model import LinearRegression
 import matplotlib as plt
+from mpl_toolkits.basemap import Basemap
+import string
+import matplotlib.cm as cm
 import seaborn as sns
 import math
 
@@ -27,5 +30,23 @@ plot.set_ylim(41.6,42.05)
 plot.set_xlim(-87.95,-87.5)
 fig = plot.get_figure()
 fig.savefig('scatter1.png')
+
+# llcrnrlat,llcrnrlon,urcrnrlat,urcrnrlon
+# are the lat/lon values of the lower left and upper right corners
+# of the map.
+# lat_ts is the latitude of true scale.
+# resolution = 'c' means use crude resolution coastlines.
+m = Basemap(projection='merc' ,llcrnrlat=41.599501,urcrnrlat=42.109914,\
+            llcrnrlon=-88.034279,urcrnrlon= -87.296822,lat_ts=20,resolution='f')
+m.drawcoastlines()
+# draw coastlines.
+m.drawcoastlines()
+# draw a boundary around the map, fill the background.
+# this background will end up being the ocean color, since
+# the continents will be drawn on top.
+m.drawmapboundary(fill_color='aqua')
+# fill continents, set lake color same as ocean color.
+m.fillcontinents(color='coral',lake_color='aqua')
+plt.show()
 
 
